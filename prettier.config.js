234bdd079f771aa906b386ee94ef178c3b6f2d44
Alexander Fu <darkestsunstudios@gmail.com>
2023-11-25 22:36:02 -0500

    Fix Restyled by using custom images

--- prettier.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').options} */
-const config = {
-  plugins: ["prettier-plugin-tailwindcss", "@trivago/prettier-plugin-sort-imports"],
-  importOrder: ["^components/(.*)$", "^[./]" ],
-  importOrderSeparation: true, 
-  importOrderSortSpecifiers: true 
-};
-
-export default config;
