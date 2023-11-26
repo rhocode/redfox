e364eb803f7fd74e8724fc572e46f08cf1c512b4
Alexander Fu <aafu@ucdavis.edu>
2023-11-25 18:16:25 -0800

    Try different type of import

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,14 +1,16 @@
-import { type Config } from "tailwindcss";
-import { fontFamily } from "tailwindcss/defaultTheme";
+// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-var-requires
+const { fontFamily } = require("tailwindcss/defaultTheme");
 
+/** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.tsx"],
   theme: {
     extend: {
       fontFamily: {
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
         sans: ["var(--font-sans)", ...fontFamily.sans],
       },
     },
   },
   plugins: [],
-} satisfies Config;
+};
