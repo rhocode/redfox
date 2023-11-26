d9370157063ddb96dc1c22ca3f547876385e1fcd
Alexander Fu <aafu@ucdavis.edu>
2023-11-25 16:33:36 -0800

    Remove other formatter test

--- prettier.config.js
+++ prettier.config.js
@@ -1,7 +1,6 @@
 /** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').options} */
 const config = {
   plugins: [
-    "prettier-plugin-organize-imports",
     "prettier-plugin-tailwindcss",
   ]
 };
