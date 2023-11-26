25df2a8810ad6f5eaa990c652b094798fddf5178
Restyled.io <commits@restyled.io>
2023-11-26 09:00:13 +0000

    Restyled by prettier

--- src/app/itinerary/[tripId]/page.tsx
+++ src/app/itinerary/[tripId]/page.tsx
@@ -1,10 +1,13 @@
 import ScheduleAccordion from "../../_components/scheduleAccordion";
 
-export default async function ItineraryPage({ params }: { params: { tripId: string } }) {
-
+export default async function ItineraryPage({
+  params,
+}: {
+  params: { tripId: string };
+}) {
   // Do something with the dynamic path
   console.log(params);
-  
+
   // TODO: add schedule accordion to item
   return (
     <>
