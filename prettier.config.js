f0a6bf8a055844111699746696d1bf95dd78acc3
Alexander Fu <aafu@ucdavis.edu>
2023-11-25 17:31:50 -0800

    Fix prettier error

--- prettier.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').options} */
-const config = {
-  plugins: [
-    "prettier-plugin-organize-imports",
-    "prettier-plugin-tailwindcss",
-  ]
-};
-
-export default config;
