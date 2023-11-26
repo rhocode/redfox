18ffa2c7d004bd4e30a71ec07a84d5daf0e2bb6e
Alexander Fu <aafu@ucdavis.edu>
2023-11-25 17:34:20 -0800

    Add bad whitespace change

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import "~/styles/globals.css";
 import { Inter } from "next/font/google";
 import { cookies } from "next/headers";
 
+
 import { TRPCReactProvider } from "~/trpc/react";
 import { ChakraProvider } from "@chakra-ui/react";
 
