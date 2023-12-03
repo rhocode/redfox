3b2380daf14b148d1a71441030317c863e9313e3
Tiffany <tiffanychien9@gmail.com>
2023-12-03 01:16:30 -0800

    Add a test page for TouristSpotCard component

---
+++ src/app/test/page.tsx
@@ -0,0 +1,10 @@
+import { Box } from "@chakra-ui/react";
+import TouristSpotCard from "../_components/TouristSpotCard";
+
+export default async function TestPage() {
+  return (
+    <Box>
+      <TouristSpotCard />
+    </Box>
+  );
+}
